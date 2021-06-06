70b863997c3c41a74952e06ed1536525202cb84c
Alexey Bobkov <alexeybobkov47@gmail.com>
2021-06-06 19:58:24 +0400

    fixed linter Error: line is 121 characters (lll)

--- internal/api/repo/pg_user_plants.go
+++ internal/api/repo/pg_user_plants.go
@@ -14,7 +14,6 @@ func (pg *PG) StorePlant(ctx context.Context, plant *models.UserPlant) (*models.
 		name, description) 
 	values ($1, $2, $3, $4, $5, $6, $7, $8, $9) 
 	returning id, next_watering, created_at;`
-	// interval := plant.WateringInterval.String()
 	err := pg.db.QueryRow(ctx, query, plant.UserID, plant.PlantReferenceID, plant.GardenID, plant.PlantingDate,
 		plant.WateringInterval, plant.LastWatering, plant.PhotoUrls, plant.Name, plant.Description).
 		Scan(&plant.ID, &plant.NextWatering, &plant.CreatedAt)
@@ -27,7 +26,8 @@ func (pg *PG) StorePlant(ctx context.Context, plant *models.UserPlant) (*models.
 	return plant, err
 }
 
-func (pg *PG) ListUserPlants(ctx context.Context, userID int64, limit int64, offset int64) ([]*models.UserPlant, error) {
+func (pg *PG) ListUserPlants(ctx context.Context,
+	userID int64, limit int64, offset int64) ([]*models.UserPlant, error) {
 	query := `SELECT id, user_id, ref_id, garden_id, planting_date, watering_interval,
 		last_watering, next_watering, photo_url, name, description,
 		created_at, modified_at
@@ -43,9 +43,10 @@ func (pg *PG) ListUserPlants(ctx context.Context, userID int64, limit int64, off
 	for rows.Next() {
 		var userPlant models.UserPlant
 		var interval time.Duration
-		err = rows.Scan(&userPlant.ID, &userPlant.UserID, &userPlant.PlantReferenceID, &userPlant.GardenID, &userPlant.PlantingDate,
-			&interval, &userPlant.LastWatering, &userPlant.NextWatering, &userPlant.PhotoUrls,
-			&userPlant.Name, &userPlant.Description, &userPlant.CreatedAt, &userPlant.ModifiedAt)
+		err = rows.Scan(&userPlant.ID, &userPlant.UserID, &userPlant.PlantReferenceID,
+			&userPlant.GardenID, &userPlant.PlantingDate, &interval, &userPlant.LastWatering,
+			&userPlant.NextWatering, &userPlant.PhotoUrls, &userPlant.Name, &userPlant.Description,
+			&userPlant.CreatedAt, &userPlant.ModifiedAt)
 		if err != nil {
 			return nil, errors.WithMessage(err, "Scan rows error: ")
 		}
