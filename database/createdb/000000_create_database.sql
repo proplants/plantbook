12858d8e571def18917025c52126aa5030b89ff2
Artem Karpov <art.frela@gmail.com>
2021-06-13 20:05:56 +0300

    Try run db-build workflow 2. :bulb:

--- database/createdb/000000_create_database.sql
+++ database/createdb/000000_create_database.sql
@@ -8,3 +8,4 @@ SELECT 'create database if not exists plantbook_admin with owner plantbook_admin
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'plantbook_admin')\gexec
 -- comment for db build
 -- same
+-- same 2
