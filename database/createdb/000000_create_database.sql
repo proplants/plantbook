143a09f3684c9d3f4e6b67fa85ac42414deea35e
Artem Karpov <art.frela@gmail.com>
2021-06-13 12:08:46 +0300

    Action on change sql for build and push image :construction_worker:
    For testing need database and easier run pod than to run migrations.

---
+++ database/createdb/000000_create_database.sql
@@ -0,0 +1,8 @@
+/*******************************************************************************
+ * 
+ *                          создание базы данных
+ * 
+ ******************************************************************************/
+-- 0. database create
+SELECT 'create database if not exists plantbook_admin with owner plantbook_admin template template1;'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'plantbook_admin')\gexec
