9dc0abd3bcc5940145dac5bf18341bc6171755fd
Artem Karpov <art.frela@gmail.com>
2021-06-13 20:44:46 +0300

    Run build database image with version :bulb:

--- database/createdb/000000_create_database.sql
+++ database/createdb/000000_create_database.sql
@@ -6,3 +6,4 @@
 -- 0. database create
 SELECT 'create database if not exists plantbook_admin with owner plantbook_admin template template1;'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'plantbook_admin')\gexec
+-- trigger for db-build
